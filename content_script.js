7093765048d672e0c0e01968966902c96f83b9ac
Victor Lai <lai.victor.vl@gmail.com>
2015-05-12 03:47:47 -0400

    Reading Video Title and Owner
    Added support to read video titles and the owner of the video, written
    to a Json string.

--- content_script.js
+++ content_script.js
@@ -1,20 +1,70 @@
-find(document);
 
-function find(doc)
+// find all the 
+saveAllVideos(document);
+
+function saveAllVideos(doc)
 {
+	videos = [];
+
+	// get all 'td' element videos in the playlist
 	var video_title_eles = doc.getElementsByClassName('pl-video-title'); 
-	for (var i = 0; i < video_title_eles.length; ++i) {
+	for (var i = 0; i < video_title_eles.length; ++i) 
+	{
+		// get the current element in the iteration
 		var video_title = video_title_eles[i];
-		console.log(video_title.innerText);
 		
+		// console.log(video_title.innerText);
+		
+		video_object = {
+			"title": "",
+			"video-link": "",
+			"owner": "",
+			"owner-link": "",
+			"playlist": "",
+		};
+
+		// get the current video's 'a' element
 		var video_title_link_eles = video_title.getElementsByClassName('pl-video-title-link');
 		if (video_title_link_eles.length > 0)
 		{
+			// select the one 'a' element
 			var video_title_link = video_title_link_eles[0];
 
-			var raw_link = video_title_link.getAttribute('href');
-			console.log(raw_link);
+			// get the 'href' link for this 'a' element
+			var video_link = video_title_link.getAttribute('href');
+
+			// split the link by the query parameters
+			var video_link_split = video_link.split('&');
+			for (var j = 0; j < video_link_split.length; ++j)
+			{
+				// console.log(video_link_split[j]);
+			}
+
+			video_object["title"] = video_title_link.innerText;
+			video_object["video-link"] = video_link_split[0];
+			video_object["playlist"] = video_link_split[2];
 		}
+
+		var video_owner_eles = video_title.getElementsByClassName('pl-video-owner');
+		if (video_owner_eles.length > 0)
+		{
+			var video_owner = video_owner_eles[0];
+
+			var video_owner_link_ele = video_owner.getElementsByTagName('a');
+			if (video_owner_link_ele.length > 0)
+			{
+				var video_owner_link = video_owner_link_ele[0];
+
+				var owner_link = video_owner_link.getAttribute('href');
+
+				video_object["owner"] = video_owner_link.innerText;
+				video_object["owner-link"] = owner_link;
+			}
+		}
+		
+		videos.push(video_object);
 	}
+
+	console.log(JSON.stringify(videos));
 }
 
